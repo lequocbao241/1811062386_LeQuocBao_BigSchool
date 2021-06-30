73bb92b999fb389435d5ab573a0e3b9e33d05c7f
lequocbao241 <82934905+lequocbao241@users.noreply.github.com>
2021-06-30 13:50:04 +0700

    Add project files.

---
+++ 1811062386_LeQuocBao_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811062386_LeQuocBao_BigSchool.MvcApplication" Language="C#" %>
---
+++ 1811062386_LeQuocBao_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811062386_LeQuocBao_BigSchool.Startup))]
+namespace _1811062386_LeQuocBao_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
